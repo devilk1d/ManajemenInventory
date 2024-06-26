d01e423af6d4edd01e5c335c1ba82e6144a09a7f
devilk1d <abimanyu123em@gmail.com>
2024-06-26 19:57:38 +0700

    Memperbaiki code yang error + memasukkan table db

---
+++ Fitur Manage Customer dan Product V2/src/dao/tables.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author abima
+ */
+public class tables {
+
+    public static void main(String[] args) {
+        Connection con = null;
+        Statement st = null;
+        try {
+            con = Koneksi.getCon();
+            st = con.createStatement();
+            //st.executeUpdate("create table appuser(appuser_pk int AUTO_INCREMENT primary key,userRole varchar(50),nama varchar(200),noTelp varchar(50),email varchar(200),password varchar(50),alamat varchar(200),status varchar(50) )");
+            //st.executeUpdate("insert into appuser (userRole,nama,noTelp,email,password,alamat,status) values('SuperAdmin','Super Admin','12345','SAdmin@MWB.com','admin','Indonesia','Active')");
+            //st.executeUpdate("create table category(category_pk int AUTO_INCREMENT primary key, name varchar(200))"); 
+            //st.executeUpdate("create table product(product_pk int AUTO_INCREMENT primary key,name varchar(200),quantity int,price int,description varchar(500),category_fk int)");
+            //st.executeUpdate("create table customer(customer_pk int AUTO_INCREMENT primary key,name varchar(200),noTelp varchar(50),email varchar(200))");
+            JOptionPane.showMessageDialog(null, "Tabel berhasil dibuat");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        } finally {
+            try {
+                con.close();
+                st.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+}
